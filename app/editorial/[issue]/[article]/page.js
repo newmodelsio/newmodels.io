4a1f75e2182e8e52d8cdcc331a42bf35a8f99872
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2024-07-29 15:51:13 +0200

    init

--- app/editorial/[issue]/[article]/page.js
+++ app/editorial/[issue]/[article]/page.js
@@ -1,5 +1,4 @@
 import Logo from "../../../components/Logo"
-import Aggregate from "../../../components/Aggregate"
 import Footer from "../../../components/Footer"
 
 export default async function Page({ params }) {
