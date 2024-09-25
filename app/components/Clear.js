67220c8fdc3f342ed63a37dee7695c494dc1212e
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2024-09-25 09:49:28 +0200

    json fix

--- app/components/Clear.js
+++ app/components/Clear.js
@@ -11,19 +11,19 @@ export default function ClearMinimal({ data }) {
           ))}
         </div>
         <div className="flex flex-col px-5 gap-5 mb-5">
-          <div className="w-full border-t "></div>
+          <div className="w-full md:border-t"></div>
           {data.columnB.map((block) => (
             <Block key={block.id} block={block} />
           ))}
         </div>
         <div className="flex flex-col px-5 gap-5 mb-5">
-          <div className="w-full border-t "></div>
+          <div className="w-full md:border-t"></div>
           {data.columnC.map((block) => (
             <Block key={block.id} block={block} />
           ))}
         </div>
       </div>
-      <div className="p-5 border-t">
+      <div className="p-5 md:border-t">
         <strong>NEWSSTAND</strong>
         <div
           className="columns-2 md:columns-3"
