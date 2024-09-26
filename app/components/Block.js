496f08fa9025612f43358143256b49ae8cc341bf
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2024-09-26 07:51:59 +0200

    embed

--- app/components/Block.js
+++ app/components/Block.js
@@ -19,13 +19,6 @@ export default function Block({ block }) {
                   dangerouslySetInnerHTML={{ __html: block.text }}
                 ></div>
               )
-            case "heading":
-              return (
-                <div
-                  className="font-bold mb-5 leading-normal"
-                  dangerouslySetInnerHTML={{ __html: block.text }}
-                ></div>
-              )
             case "image":
               return (
                 <div className="flex flex-wrap">
@@ -60,6 +53,20 @@ export default function Block({ block }) {
               return <Post block={block} />
             case "hr":
               return <hr />
+
+            case "heading":
+              return (
+                <div
+                  className="font-bold mb-5 leading-normal"
+                  dangerouslySetInnerHTML={{ __html: block.text }}
+                ></div>
+              )
+            case "embed":
+              return (
+                <div>
+                  <div dangerouslySetInnerHTML={{ __html: block.iframe }}></div>
+                </div>
+              )
             default:
               return null
           }
