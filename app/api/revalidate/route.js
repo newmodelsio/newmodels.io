a869bd0ead2718f2469bebe34dd2d6ae30b903e5
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2024-10-03 12:38:21 +0200

    tag

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server"
 import { revalidatePath } from "next/cache"
+import { revalidateTag } from "next/cache"
 
 export async function GET(request) {
-  revalidatePath("/")
+  revalidateTag("kirby")
   return NextResponse.json({ msg: "Hello from server" })
 }
