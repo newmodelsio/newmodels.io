e1a2924e9fb54f434cbc61fbf25ef4bd293bd8e3
JonL <office@jon-l.com>
2025-12-08 18:03:33 +0100

    revalidate tag

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -3,6 +3,8 @@ import { revalidatePath } from "next/cache"
 import { revalidateTag } from "next/cache"
 
 export async function GET(request) {
-  revalidateTag("kirby")
+  revalidateTag("kirby", "max")
   return NextResponse.json({ msg: "Hello from server" })
 }
+
+
