798a8b0029c135e7669dd9173c5a9d3f5b73a2fb
JonL <office@jon-l.com>
2025-12-09 14:15:16 +0100

    viewport meta

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ export const metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  // maximumScale: 1,
+  // userScalable: false,
 }
 
 export default function RootLayout({ children }) {
