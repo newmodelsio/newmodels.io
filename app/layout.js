f3cac3de85dc4cc156a0c0f669a6ef10029dfa14
JonL <office@jon-l.com>
2026-01-02 18:24:43 +0100

    year

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const viewport = {
   width: "device-width",
   initialScale: 1,
   // maximumScale: 1,
-  // userScalable: false,
+  userScalable: false,
 }
 
 export default function RootLayout({ children }) {
